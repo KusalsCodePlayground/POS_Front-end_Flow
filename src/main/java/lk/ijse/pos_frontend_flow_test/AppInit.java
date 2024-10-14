cf9deefafff14ce2ba69a185e2b4c6f58ce585b4
KusalDemo <kusalgunasekara2002@gmail.com>
2024-10-15 02:03:07 +0530

    Initial Commit

---
+++ src/main/java/lk/ijse/pos_frontend_flow_test/AppInit.java
@@ -0,0 +1,13 @@
+package lk.ijse.pos_frontend_flow_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInit {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInit.class, args);
+	}
+
+}
